3608e2f86eeca8f76262c2b4f9b1fb4b487408f5
Jimmy Lv <jimmy.jinglv@gmail.com>
2018-06-02 16:57:05 +0800

    feat($each): [#0] - added jest each for testing each

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
 import { shallow } from 'enzyme'
+import each from 'jest-each'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
@@ -27,6 +28,41 @@ describe('App Component', () => {
     expect(wrapper.find('h2').text()).toEqual('2')
   })
   it('should extend Array is fine', () => {
-    expect([]).toBeArray();
+    expect([]).toBeArray()
   })
+})
+
+describe('just use jest-each to testing multiple cases', () => {
+  each`
+  a    | b    | expected
+  ${1} | ${1} | ${2}
+  ${1} | ${2} | ${3}
+  ${2} | ${1} | ${3}
+  ${2} | ${2} | ${4}
+`.it('returns $expected when adding $a to $b', ({ a, b, expected }) => {
+    expect(a + b).toBe(expected)
+  })
+
+})
+
+function isPalindrome(word) {
+  const isReversedWord = word.split('').reverse().join('') === word
+  return word.trim().length > 0 && isReversedWord
+}
+
+describe('testing for each one ', () => {
+  const cases = [
+    ['mom', true],
+    ['dad', true],
+    ['jimmy', false],
+    [' ', false],
+    ['', false],
+  ]
+
+  cases.forEach(([word, result]) => {
+    it(`should return ${result} when given argument ${word}`, () => {
+      expect(isPalindrome(word)).toBe(result)
+    })
+  })
+
 })
