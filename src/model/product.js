5f4413c4b7a1ca1f61f8646d2048dc9ac3e0811c
Jimmy Lv <jimmy.jinglv@gmail.com>
2018-06-02 23:28:02 +0800

    refactor($products): [#0] - add new product logic

--- src/model/product.js
+++ src/model/product.js
@@ -1,3 +1,18 @@
 export function totalPrice(products = []) {
   return products.reduce((prev, item) => prev + item.price * item.count, 0)
 }
+
+export const generateProduct = code => ({
+  code,
+  count: 1,
+  price: 100,
+})
+
+export function addNewProduct(products, productToAdd) {
+  const index = products.findIndex((p) => p.code === productToAdd.code)
+  if (index >= 0) {
+    products[index].count += 1
+    return products
+  }
+  return [...products, productToAdd]
+}
--- src/model/product.test.js
+++ src/model/product.test.js
@@ -1,15 +1,15 @@
-import { totalPrice } from './product'
+import { totalPrice, addNewProduct } from './product'
 
 describe('product functions', () => {
   const products = [
-    { code: '1234', name: 'AirPods', price: 1288, count: 1 },
-    { code: '4321', name: 'BeatsX', price: 1188, count: 1 },
+    { code: '1234', price: 100, count: 1 },
+    { code: '4321', price: 200, count: 1 },
   ]
 
   it('should return total price when given products', () => {
     const price = totalPrice(products)
 
-    expect(price).toBe(2476)
+    expect(price).toBe(300)
   })
 
   it('should return 0 when given empty products', () => {
@@ -23,4 +23,27 @@ describe('product functions', () => {
 
     expect(price).toBe(0)
   })
-});
+
+  it('should add new product into products', () => {
+    const productToAdd = { code: '4321', price: 200, count: 1 }
+    const products = [{ code: '1234', price: 100, count: 1 }]
+
+    const expectedProducts = addNewProduct(products, productToAdd)
+
+    expect(expectedProducts).toEqual([
+      { code: '1234', price: 100, count: 1 },
+      { code: '4321', price: 200, count: 1 },
+    ])
+  })
+
+  it('should increase product count when adding same product', () => {
+    const productToAdd = { code: '1234', price: 100, count: 1 }
+    const products = [{ code: '1234', price: 100, count: 1 }]
+
+    const expectedProducts = addNewProduct(products, productToAdd)
+
+    expect(expectedProducts).toEqual([
+      { code: '1234', price: 100, count: 2 },
+    ])
+  })
+})
