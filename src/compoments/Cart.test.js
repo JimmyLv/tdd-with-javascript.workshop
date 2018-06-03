64ae48dbe64d55fbb820c82e2e7390cc6c5b8d66
Jimmy Lv <jimmy.jinglv@gmail.com>
2018-06-03 12:46:51 +0800

    refactor($header): [#0] - Just fixed failed test and add stories and storyshot for Header

--- src/compoments/Cart.test.js
+++ src/compoments/Cart.test.js
@@ -1,22 +1,22 @@
 import { mount, shallow } from 'enzyme'
 import React from 'react'
-import { totalPrice, addNewProduct, generateProduct } from '../model/product'
+import { addNewProduct, generateProduct, totalPrice } from '../model/product'
 import Cart from './Cart'
 import Header from './Header'
 import Product from './Product'
 
 jest.mock('../model/product', () => ({
   totalPrice: jest.fn(() => 300),
-  addNewProduct: jest.fn(),
+  addNewProduct: jest.fn(() => []),
   generateProduct: jest.fn(),
 }))
 
 describe('Cart components', () => {
-  beforeEach(() => {
+  afterEach(() => {
     totalPrice.mockClear()
     addNewProduct.mockClear()
     generateProduct.mockClear()
-  });
+  })
   const products = [
     { code: '1234', price: 100, count: 1 },
     { code: '4321', price: 200, count: 1 },
@@ -43,14 +43,16 @@ describe('Cart components', () => {
     expect(wrapper.find('.price').text()).toBe('总价：300')
   })
 
-
   it('should invoke handleAddProduct when click the confirm button', () => {
     const wrapper = mount(<Cart />)
     const spy = jest.spyOn(wrapper.instance(), 'handleAddProduct')
     wrapper.instance().forceUpdate()
 
     expect(spy).toHaveBeenCalledTimes(0)
-    wrapper.find('form').simulate('submit', { target: { value: '1234' } })
+
+    wrapper.find('input').simulate('change', { target: { value: '1234' } })
+    wrapper.find('form').simulate('submit')
+
     expect(spy).toHaveBeenCalledTimes(1)
   })
 
---
+++ src/compoments/Header.stories.js
@@ -0,0 +1,8 @@
+import { storiesOf } from '@storybook/react'
+import React from 'react'
+import Header from './Header'
+
+storiesOf('Header', module)
+  .add('with title', () =>
+    <Header title="Hello World!"/>,
+  )
