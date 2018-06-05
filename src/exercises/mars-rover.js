efbb53dc3ed3e0e226526b636bc23136deb2c72a
Jimmy Lv <jimmy.jinglv@gmail.com>
2018-06-05 18:08:30 +0800

    feat($mars-rover): [#0] - just add mars-rover case

---
+++ src/exercises/mars-rover.js
@@ -0,0 +1,64 @@
+const LOWER_BOUNDARY = 0
+const UPPER_BOUNDARY = 5
+
+const VECTORS = {
+  'N': { offsetX: 0, offsetY: 1 },
+  'E': { offsetX: 1, offsetY: 0 },
+  'S': { offsetX: 0, offsetY: -1 },
+  'W': { offsetX: -1, offsetY: 0 },
+}
+const DIRECTIONS = Object.keys(VECTORS)
+
+const COMMANDS = ['M', 'L', 'R']
+
+function parseCommand(rawCommands) {
+  if (!rawCommands) {
+    throw new Error('Please input commands!')
+  }
+
+  const commands = rawCommands.split('')
+  const validCommands = commands.filter(command => COMMANDS.includes(command))
+  const notAllValidCommands = validCommands.length !== rawCommands.length
+
+  if (notAllValidCommands) {
+    throw new Error('Invalid commands!')
+  }
+  return commands
+}
+
+function limit(number) {
+  if (number > UPPER_BOUNDARY) return UPPER_BOUNDARY
+  if (number < LOWER_BOUNDARY) return LOWER_BOUNDARY
+  return number
+}
+
+class MarsRover {
+  constructor(x, y, direction) {
+    this.x = x
+    this.y = y
+    this.direction = direction
+  }
+
+  executors = {
+    'M': () => {
+      const { offsetX, offsetY } = VECTORS[this.direction]
+      this.x = limit(this.x + offsetX)
+      this.y = limit(this.y + offsetY)
+    },
+    'L': () => this.direction = DIRECTIONS[(DIRECTIONS.indexOf(this.direction) + 3) % 4],
+    'R': () => this.direction = DIRECTIONS[(DIRECTIONS.indexOf(this.direction) + 1) % 4],
+  }
+
+  execute(commands) {
+    parseCommand(commands)
+      .map(command => this.executors[command])
+      .forEach(executor => executor())
+    return this.status()
+  }
+
+  status() {
+    return `${this.x} ${this.y} ${this.direction}`
+  }
+}
+
+export default MarsRover
---
+++ src/exercises/mars-rover.test.js
@@ -0,0 +1,103 @@
+import MarsRover from './mars-rover'
+// import parseCommand from './parse-command'
+//
+// jest.mock('./parse-command', () => jest.fn().mockReturnValue('M'))
+
+describe('MarsRover', () => {
+  describe('status', () => {
+    it('should return status of marsRover when given initial position and direction', () => {
+      const marsRover = new MarsRover(0, 0, 'N')
+
+      const status = marsRover.status()
+
+      expect(status).toBe('0 0 N')
+    })
+
+    it('should throw exception when marsRover get position out of range', () => {
+      // expect(() => new MarsRover(-1, 6, 'N')).toThrowError('Invalid position!')
+    })
+
+    it('should throw exception when receive empty command', () => {
+      const marsRover = new MarsRover(0, 0, 'N')
+
+      expect(() => marsRover.execute(undefined)).toThrowError('Please input commands!')
+    })
+    it('should throw exception when receive invalid command', () => {
+      const marsRover = new MarsRover(0, 0, 'N')
+
+      expect(() => marsRover.execute('Unknown')).toThrowError('Invalid commands!')
+    })
+  });
+
+  describe('single command', () => {
+    it('should move forward to North when receive one M command', () => {
+      const marsRover = new MarsRover(0, 0, 'N')
+
+      const status = marsRover.execute('M')
+
+      expect(status).toBe('0 1 N')
+    })
+
+    it('should move forward to East when receive one M command', () => {
+      const marsRover = new MarsRover(0, 0, 'E')
+
+      const status = marsRover.execute('M')
+
+      expect(status).toBe('1 0 E')
+    })
+  })
+
+  describe('turn command', () => {
+    it('should turn left when receive L command', () => {
+      const marsRover = new MarsRover(0, 0, 'N')
+
+      const status = marsRover.execute('L')
+
+      expect(status).toBe('0 0 W')
+    })
+
+    it('should turn right when receive R command', () => {
+      const marsRover = new MarsRover(0, 0, 'N')
+
+      const status = marsRover.execute('R')
+
+      expect(status).toBe('0 0 E')
+    })
+  });
+
+  describe('multiple commands', function () {
+    it('should move back when receive multiple command', () => {
+      const marsRover = new MarsRover(1, 1, 'N')
+
+
+      const status = marsRover.execute('RRM')
+
+      expect(status).toBe('1 0 S')
+    })
+
+    it('should return when receive multiple command', () => {
+      const marsRover = new MarsRover(1, 1, 'N')
+
+      const status = marsRover.execute('MLRLM')
+
+      expect(status).toBe('0 2 W')
+    })
+  })
+
+  describe('boundary', function () {
+    it('should not across 0 when receive move command', () => {
+      const marsRover = new MarsRover(0, 0, 'W')
+
+      const status = marsRover.execute('M')
+
+      expect(status).toBe('0 0 W')
+    })
+    it('should not across 5 when receive move command', () => {
+      const marsRover = new MarsRover(0, 5, 'N')
+
+      const status = marsRover.execute('M')
+
+      expect(status).toBe('0 5 N')
+    })
+  })
+})
