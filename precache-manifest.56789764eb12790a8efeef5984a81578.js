c8f6a77b859e0334d64ab8fb80b78c59f603e88b
JimmyLv <jimmy.jinglv@gmail.com>
2019-09-02 18:10:47 +0800

    Updates

---
+++ precache-manifest.56789764eb12790a8efeef5984a81578.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tdd-with-javascript.workshop/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "712750ed011d1b40393f",
+    "url": "/tdd-with-javascript.workshop/static/js/runtime~main.4a61458a.js"
+  },
+  {
+    "revision": "4ff67b71159b0400f06b",
+    "url": "/tdd-with-javascript.workshop/static/js/main.c74e6b64.chunk.js"
+  },
+  {
+    "revision": "2b1d3725f22be927ff3f",
+    "url": "/tdd-with-javascript.workshop/static/js/2.78e84862.chunk.js"
+  },
+  {
+    "revision": "4ff67b71159b0400f06b",
+    "url": "/tdd-with-javascript.workshop/static/css/main.dc282dd7.chunk.css"
+  },
+  {
+    "revision": "05c16825aba22922cc63efa90feb00e3",
+    "url": "/tdd-with-javascript.workshop/index.html"
+  }
+];
